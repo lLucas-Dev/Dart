ab7540e065cfd575038396005ddbe9b311d4ff4b
Lucas <lucasllinsphb@gmail.com>
2026-06-07 10:32:29 -0300

    feat: add main and media calculation functionality for average grades

---
+++ Media das Notas/main.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'media.dart';
+
+void main(){
+    double nota1 = 0;
+    double nota2 = 0;
+    double nota3 = 0; 
+
+    print("Digite a primeira nota: ");
+    nota1 = double.parse(stdin.readLineSync()!);
+    print("Digite a segunda nota: ");
+    nota2 = double.parse(stdin.readLineSync()!);
+    print("Digite a terceira nota: ");
+    nota3 = double.parse(stdin.readLineSync()!);
+
+    double media = mediaNotas(nota1, nota2, nota3);
+    print("A média das notas é: $media");
+}
---
+++ Media das Notas/media.dart
@@ -0,0 +1,4 @@
+double mediaNotas(double nota1, double nota2, double nota3){
+  double media = (nota1 + nota2 + nota3) / 3;
+  return media;
+}
